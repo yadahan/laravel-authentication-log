d6b304ee4dc83fd5fb7b08f9537411e69d9ac757
Samuel Nitsche <mail@samynitsche.de>
2019-04-28 14:42:37 +0200

    Add queueing of notifications

--- src/Notifications/NewDevice.php
+++ src/Notifications/NewDevice.php
@@ -4,12 +4,13 @@ namespace Yadahan\AuthenticationLog\Notifications;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Yadahan\AuthenticationLog\AuthenticationLog;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Messages\NexmoMessage;
 use Illuminate\Notifications\Messages\SlackMessage;
 
-class NewDevice extends Notification
+class NewDevice extends Notification implements ShouldQueue
 {
     use Queueable;
 
