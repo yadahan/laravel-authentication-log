ec122c6e9b1d688230939068931ffde1cf5a7790
KeyShang <arthur2sereno@gmail.com>
2021-06-08 17:40:24 +0800

    fix: The trait Illuminate\Notifications\Notifiable requires the property $email which is not provided

--- src/Phpdoc/User.php
+++ src/Phpdoc/User.php
@@ -7,6 +7,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use KeyShang\AuthenticationLog\AuthenticationLogable;
 
+/**
+ * Class for phpdoc
+ *
+ * @property string $email
+ */
 class User extends Authenticatable
 {
     use HasFactory;
