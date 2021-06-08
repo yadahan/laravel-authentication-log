ffcf05ab7d75c88684e3cc8444c0a83077925d76
KeyShang <arthur2sereno@gmail.com>
2021-06-08 17:58:18 +0800

    fix phpdoc

--- src/Phpdoc/User.php
+++ src/Phpdoc/User.php
@@ -5,12 +5,15 @@ namespace KeyShang\AuthenticationLog\Phpdoc;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Carbon;
 use KeyShang\AuthenticationLog\AuthenticationLogable;
 
 /**
  * Class for phpdoc
  *
  * @property string $email
+ * @property Carbon $login_at
+ * @property string $ip_address
  */
 class User extends Authenticatable
 {
