1821d757a3382103aa8d2706d4fbbe157f42537f
Pushkraj Jori <pushkrajjori19@gmail.com>
2023-05-08 15:20:26 +0530

    Update EventMap.php

--- src/EventMap.php
+++ src/EventMap.php
@@ -19,7 +19,7 @@ trait EventMap
         ],
         
         'Illuminate\Auth\Events\OtherDeviceLogout' => [
-            'Yadahan\AuthenticationLog\Listeners\LogSuccessfulLogout',
+            'Yadahan\AuthenticationLog\Listeners\LogSuccessfulOtherDeviceLogout',
         ],
     ];
 }
