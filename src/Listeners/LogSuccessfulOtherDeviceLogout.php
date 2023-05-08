c33d69d0a13f9754bddc7bfd3e0e28d3a9fe4404
Pushkraj Jori <pushkrajjori19@gmail.com>
2023-05-08 15:19:37 +0530

    Create LogSuccessfulOtherDeviceLogout.php

---
+++ src/Listeners/LogSuccessfulOtherDeviceLogout.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Yadahan\AuthenticationLog\Listeners;
+
+use Illuminate\Auth\Events\Logout;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Yadahan\AuthenticationLog\AuthenticationLog;
+
+class LogSuccessfulOtherDeviceLogout
+{
+    /**
+     * The request.
+     *
+     * @var \Illuminate\Http\Request
+     */
+    public $request;
+
+    /**
+     * Create the event listener.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  Logout  $event
+     * @return void
+     */
+    public function handle(Logout $event)
+    {
+        if ($event->user) {
+            $user = $event->user;
+            $ip = $this->request->ip();
+            $userAgent = $this->request->userAgent();
+            $authenticationLog = $user->authentications()->whereIpAddress($ip)->whereUserAgent($userAgent)->first();
+
+            if (! $authenticationLog) {
+                $authenticationLog = new AuthenticationLog([
+                    'ip_address' => $ip,
+                    'user_agent' => $userAgent,
+                ]);
+            }
+
+            $authenticationLog->logout_at = Carbon::now();
+
+            $user->authentications()->save($authenticationLog);
+        }
+    }
+}
